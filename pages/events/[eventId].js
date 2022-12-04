cd731989f25422599c69ebf6107880a29227b8ef
francopoffo <franco_sutter@yahoo.com.br>
2022-12-04 17:59:50 -0300

    Corrigindo bug quando não encontrava evento

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -4,15 +4,29 @@ import { Fragment } from "react";
 import EventSumarry from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
+import Button from "../../components/ui/button";
 
 function EventDetail() {
   const router = useRouter();
 
   const eventId = router.query.eventId;
   const event = getEventById(eventId);
+  console.log(event);
 
   if (!event) {
-    <p>O evento não foi encontrado.</p>;
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Nenhum evento foi encontrado.</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button className="center" link="/events">
+            Todos os eventos
+          </Button>
+        </div>
+      </Fragment>
+    );
   }
 
   return (
