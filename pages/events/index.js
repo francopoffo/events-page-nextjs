2ee18760705ade0056a943b8f48e4ea56c9c2144
Franco Poffo <franco_sutter@yahoo.com.br>
2023-02-06 16:52:11 -0300

    Otimizando imagens e adicionando meta tags

--- pages/events/index.js
+++ pages/events/index.js
@@ -3,6 +3,7 @@ import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-seach";
 import { Fragment } from "react";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 function Events(props) {
   const events = props.events;
@@ -16,6 +17,9 @@ function Events(props) {
 
   return (
     <Fragment>
+      <Head>
+        <title>Todos os eventos</title>
+      </Head>
       <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
+import Head from "next/head";
 
 function Home(props) {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Descubra eventos que te ajudam a crescer"
+        />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
